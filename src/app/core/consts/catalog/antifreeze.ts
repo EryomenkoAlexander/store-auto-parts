752572619ccb8bb2caf055d2f4bc5911a1cc5a38
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2024-06-12 22:51:40 +0600

    fix: images

--- src/app/core/consts/catalog/antifreeze.ts
+++ src/app/core/consts/catalog/antifreeze.ts
@@ -7,7 +7,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Тосол ГОСТОВСКИЙ (-40)   1 кг',
     category: CategoryEnum.antifreeze,
     price: 5000,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/%D0%B3%D0%BE%D1%81%D1%82%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9/978589/ac08fc26-4de0-4e10-be39-84c5a4d4466b_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240425%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240425T033439Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=751a301eddc421fe63d73f5bbd6cc294a52f3671355699af72b1134c3a7f4adf',
+    imagePath: 'https://images.satu.kz/185736783_antifriz-nord-zelyonyj.jpg',
     count: 0
   },
   {
@@ -15,7 +15,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз UNIX зеленый G11 1кг',
     category: CategoryEnum.antifreeze,
     price: 4500,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/unix/4602507/1ee046a8-8cab-498d-88b2-8d47c97a3dfb_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013408Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=3a9f9e05851226ca286b7c531238b9700d330d4f7ae589f8d5c57e48d64616ff',
+    imagePath: 'https://ir-3.ozone.ru/s3/multimedia-w/c1000/6200365736.jpg',
     count: 0
   },
   {
@@ -23,7 +23,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз ГОСТОВСКИЙ (-40) красный  1 кг',
     category: CategoryEnum.antifreeze,
     price: 5700,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/%D0%B3%D0%BE%D1%81%D1%82%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9/803510/281fdfdf-d1d2-4afd-9a35-d79a6f013ec1_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013358Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=819b0475b5d892a07a8d5dc065e0901f7ef4cecc6b14fd8339538459cfc6c84b',
+    imagePath: 'https://ir-3.ozone.ru/s3/multimedia-v/c1000/6258447847.jpg',
     count: 0
   },
   {
@@ -31,7 +31,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз UNIX желтый G12 1кг',
     category: CategoryEnum.antifreeze,
     price: 6500,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/%D0%B3%D0%BE%D1%81%D1%82%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9/804114/088c91bf-064b-4409-8483-6e04d2d46891_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013354Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=27dbdaae8a92256a5e344909b0f6dceb195cc0c96fbbadab6cefa409c2e88b5b',
+    imagePath: 'https://dav.kz/image/cache/catalog/product/Noname/antifreeze-zcool-red-10kg-1000x1000.jpg',
     count: 0
   },
   {
@@ -39,7 +39,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз НИАГАРА (-40) синий G11 1кг',
     category: CategoryEnum.antifreeze,
     price: 6800,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/unix/4603214/ad34563d-2d83-4eb6-8c7b-bbf9841d9dcb_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240425%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240425T033334Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=698a6a1705fa18dea7e36c6ed3b102e658a46858739d00d17a81338cace4fa12',
+    imagePath: 'https://ir-3.ozone.ru/s3/multimedia-t/c1000/6652145945.jpg',
     count: 0
   },
   {
@@ -47,7 +47,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз UNIX красный G12 1кг',
     category: CategoryEnum.antifreeze,
     price: 6800,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/niagara/001001003006/b5adff0e-d6e1-4d87-9601-b38951239038_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T051709Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=eb4244917b2c33b5f45bb59dda723788eb35f7c656b2867d31769d3a217e66bf',
+    imagePath: 'https://images.satu.kz/164523710_antifriz-winkod-87kg.jpg',
     count: 0
   },
   {
@@ -55,7 +55,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз НИАГАРА (-40) зеленый G11 1кг',
     category: CategoryEnum.antifreeze,
     price: 7500,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/%D0%B3%D0%BE%D1%81%D1%82%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9/978589/ac08fc26-4de0-4e10-be39-84c5a4d4466b_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240425%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240425T033439Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=751a301eddc421fe63d73f5bbd6cc294a52f3671355699af72b1134c3a7f4adf',
+    imagePath: 'https://ir-3.ozone.ru/s3/multimedia-o/c1000/6594234216.jpg',
     count: 0
   },
   {
@@ -63,7 +63,7 @@ export const Antifreeze: CatalogItemInterface[] = [
     description: 'Антифриз SIBIRIA (-40) синий G11   1 кг',
     category: CategoryEnum.antifreeze,
     price: 7600,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/niagara/001001002006/3ee77b43-6ce1-44ce-bb9f-797867f19de8_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013348Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=29fd9cf479c2ba8581866c032aa29680c93656eaef1019178e6b3f2c1534bbd8',
+    imagePath: 'https://6st.kz/image/cache/catalog/cat-antifreeze/21691-1000x1000.jpg',
     count: 0
   },
 ]
--- src/app/core/consts/catalog/motorOils.ts
+++ src/app/core/consts/catalog/motorOils.ts
@@ -7,7 +7,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное LUXE ART 15W-40 SG/CD минеральное "Стандарт" 1 л',
     category: CategoryEnum.motorOils,
     price: 8000,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/luxe/362/8dc2640f-d006-417c-8a2d-eab790d1ebbe_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013206Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=e8f0c2273a09628cfdac23a25228b84c24d6711e9ed68b818d9c5513af75e5da'
+    imagePath: 'https://images.satu.kz/125409440_w600_h600_125409440.jpg'
   ,
     count: 0
   },
@@ -16,7 +16,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное LUXE PR 15W-40 SJ/CF минеральное  "Супер" 1 л',
     category: CategoryEnum.motorOils,
     price: 8500,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/luxe/309/5e1f007c-b8fe-4b67-8717-921aa3d58b21_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013154Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=d41126202ec57c1edc46021e0bf2d9705665f9a050af75276872e1be8cf139e3'
+    imagePath: 'https://maslenka.kz/wp-content/uploads/2017/04/5w30-4L.jpg'
   ,
     count: 0
   },
@@ -25,7 +25,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное LUXE "LUX" 5W-30 SJ/CF полусинтетика 1 л',
     category: CategoryEnum.motorOils,
     price: 9000,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/luxe/103/89228b89-0e85-413d-9852-2884a7541f0b_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013145Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=e98546ced583b4c1bd44105a4ae37748420fd39115521d0988ef294dacd65092'
+    imagePath: 'https://kz.lukoil-shop.com/upload/iblock/def/defafe18cd0f320e4c2b09f619ec96d6.jpg'
   ,
     count: 0
   },
@@ -34,7 +34,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное LUKOIL СУПЕР   10W-40  SG/CD 1 л',
     category: CategoryEnum.motorOils,
     price: 11000,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/lukoil/19191/fcffd057-6ed8-40f9-9011-b39ab9ed0dd9_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013111Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=c8d75ebd07e13f1c68d358687c1f88e9707ca4abaf8589435a3e9a9538dab82e'
+    imagePath: 'https://images.satu.kz/108649294_w600_h600_108649294.jpg'
   ,
     count: 0
   },
@@ -43,7 +43,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное Gazpromneft Standart SF/CC 10W-40 минеральное 1 л',
     category: CategoryEnum.motorOils,
     price: 8400,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/gazpromneft/253142160/c997ce1e-af7b-46bf-b45d-4df59ab8e312_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013045Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=71f05ee136c098737b53546e6f16e044a5cb3f81e6c8a523c12b019b8ee60b46'
+    imagePath: 'https://auto-product.kz/wp-content/uploads/2022/12/zinol-sp-4l-350x350.png'
   ,
     count: 0
   },
@@ -52,7 +52,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Масло моторное SINTEC 10W40 Супер API SG/CD 1л п/син',
     category: CategoryEnum.motorOils,
     price: 9100,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/sintec/801893/5c5bc80a-b902-4d09-9ebe-93fc47263346_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013015Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=5713bd242b83168bc10127bc3c67974f860d1802f7707f59e753b5f089c67a34'
+    imagePath: 'https://images.satu.kz/65102115_w600_h600_65102115.jpg'
   ,
     count: 0
   },
@@ -61,7 +61,7 @@ export const MotorOils: CatalogItemInterface[] = [
     description: 'Моторное масло Luxe SL 10W-40 SG/CD 1л',
     category: CategoryEnum.motorOils,
     price: 10000,
-    imagePath: 'https://s3-leonet-market.storage.yandexcloud.net/lukoil/19191/fcffd057-6ed8-40f9-9011-b39ab9ed0dd9_thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEkvIJit9Tk0PHnY-pwf_w%2F20240422%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240422T013111Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=c8d75ebd07e13f1c68d358687c1f88e9707ca4abaf8589435a3e9a9538dab82e'
+    imagePath: 'https://images.satu.kz/104147844_w600_h600_104147844.jpg'
   ,
     count: 0
   },
